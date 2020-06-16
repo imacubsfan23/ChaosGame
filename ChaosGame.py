d06644a5650e6f404755e223e1f673c31827e684
imacubsfan23 <imacubsfan23@gmail.com>
2020-06-15 20:32:34 -0500

    rewrote the program with better naming scheme

--- ChaosGame.py
+++ ChaosGame.py
@@ -8,86 +8,78 @@ size = width, height = 600, 600
 screen = pygame.display.set_mode(size)
 
 white = (255,255,255)
-red = (255,0,0)
+red   = (255,0,0)
+green = (0,255,0)
 black = (0, 0, 0)
 
 class Dot():
-    def __init__(self, x, y, color, id=10):
+    def __init__(self, x, y, color):
         self.rect = pygame.Rect(3,3,3,3)
         self.rect.x = int(x)
         self.rect.y = int(y)
-        self.id = dots
         self.color = color
-        if self.id <= dots:
-            all_dots.append(self)
 
 def DrawWindow():
     screen.fill(black)
 
-def getVertices():
-    return int(input("How many vertices do you want? "))
+def getNumOfVertices():
+    v = int(input("How many vertices do you want? "))
+    return v if v > 2 else getNumOfVertices()
 
 def quit():
     pygame.quit()
     sys.exit()
     wait()
 
-all_dots = []
-placed = []
-dots = getVertices()
-dotcount = dots
-tdistance = 1/(dotcount-1)
+const_num_of_vertices = getNumOfVertices()
+num_of_vertices = const_num_of_vertices
+vertices = []
+generated_dots = []
+DrawWindow()
 
-for dot in range(dots):
-    print("{} {} left to place.".format(dots, 'vertices' if dots>1 else 'vertex'))
-    dot_placed = False
-    while not dot_placed:
+for vertex in range(num_of_vertices):
+    print("{} {} left to place.".format(num_of_vertices, 'vertices' if num_of_vertices>1 else 'vertex'))
+    vertex_placed = False
+    while not vertex_placed:
         pygame.event.clear()
         event = pygame.event.wait()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = event.pos
-            Dot(x, y, red, dots)
-            dots -= 1
-            dot_placed = True
         if event.type == pygame.QUIT: quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            v = Dot(event.pos[0], event.pos[1], red)
+            vertices.append(v)
+            pygame.draw.rect(screen, v.color, v.rect)
+            num_of_vertices -= 1
+            vertex_placed = True
+    pygame.display.flip()
 
 print("Click for Starting Point")
-startpoint = False
-while not startpoint:
+startpoint_placed = False
+startpoint = None
+while not startpoint_placed:
     pygame.event.clear()
     event = pygame.event.wait()
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        startx, starty = event.pos
-        Dot(startx, starty, white, dots)
-        startpoint = True
     if event.type == pygame.QUIT: quit()
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        startpoint = Dot(event.pos[0], event.pos[1], white)
+        pygame.draw.rect(screen, startpoint.color, startpoint.rect)
+        startpoint_placed = True
+pygame.display.flip()
 
 running = True
-lastpos = [startx, starty]
-place = 100000
+lastpos = [startpoint.rect.x, startpoint.rect.y]
 while running:
-    choice = random.randint(1, dotcount)
-    chosen = None
-    for dot in all_dots:
-        if dot.id == choice:
-            chosen = dot
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
 
-    if place > 0:
-        x = (chosen.rect.x + int(lastpos[0]))*tdistance
-        y = (chosen.rect.y + int(lastpos[1]))*tdistance
-        Dot(x, y, white)
-        place -= 1
-
+    random_vertex = vertices[random.randint(0, const_num_of_vertices-1)]
+    x = (random_vertex.rect.x + lastpos[0])/(const_num_of_vertices-1)
+    y = (random_vertex.rect.y + lastpos[1])/(const_num_of_vertices-1)
     lastpos[0] = x
     lastpos[1] = y
-
-    for dot in all_dots:
-        pygame.draw.rect(screen, (dot.color), dot.rect)
-    for dot in placed:
-        pygame.draw.rect(screen, (dot.color), dot.rect)
+    dot = Dot(x, y, white)
+    pygame.draw.rect(screen, dot.color, dot.rect)
+    #print("x:{}, y:{}".format(x,y))
+    #pygame.time.delay(1000)
 
     pygame.display.flip()
-    DrawWindow()
